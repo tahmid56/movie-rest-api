6561daba146b2fee39315ee5b301dda64cb5e0d5
Tahmid Akter <tahmidakter56@gmail.com>
2024-08-08 06:42:17 +0600

    middleware added for recovery, errors added, updated responses

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
+
 	"github.com/julienschmidt/httprouter"
 )
+
+type envelope map[string]any
 
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
@@ -14,4 +18,19 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 		return 0, errors.New("invalid id parameter")
 	}
 	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	js = append(js, '\n')
+	for key, value := range headers{
+		w.Header()[key] = value
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+	return nil
 }
